81ae7c162f5e93816a19dbe3b8eff4fa8e1ac6f4
maxigeist <geist.maximo@gmail.com>
2023-08-07 14:19:13 -0300

    Training finished

--- src/service/apis.js
+++ src/service/apis.js
@@ -7,9 +7,13 @@ const trainingAxios = axios.create({
 export const getComments = async () => {
     const response = await trainingAxios.get("")
     return response.data.slice(0, 5)
-}
+}  
 
 export const getComment = async (id) => {
+    const response = await trainingAxios.get(id)
+    console.log(response.data)
+    return response.data
+
     /**
      * TODO Obtener el comentario por id
      *
